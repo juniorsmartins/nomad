8cdf9a0b2f795bd4830f2a2d43edfd3f2367b52f
juniorsmartins <junior.soares.martins@proton.me>
2024-12-01 13:06:26 -0400

    FEAT: add projeto AccountingAnalysis

---
+++ accounting_analysis/src/main/java/com/nomad/accounting_analysis/AccountingAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.accounting_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingAnalysisApplication.class, args);
+	}
+
+}
