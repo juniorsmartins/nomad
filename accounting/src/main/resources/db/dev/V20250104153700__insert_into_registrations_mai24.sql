7044f6affa7eff5b2e64ee1b3841c6fc0c15d2c7
juniorsmartins <junior.soares.martins@proton.me>
2025-01-04 16:26:49 -0300

    FEAT: add dados

---
+++ accounting/src/main/resources/db/dev/V20250104153700__insert_into_registrations_mai24.sql
@@ -0,0 +1,34 @@
+INSERT INTO registrations(
+    registration_id, cashbook_id, description, amount, type_operation, date_operation, cost_center, supplier
+)
+VALUES
+    ('fba18b6a-eed9-4887-8920-80aee88070d8', 'c111de6e-396c-4940-bd95-394af78076db', 'Ração', 138.29, 'OUTPUT', '2024-05-02', 'PET', 'Cobasi'),
+    ('d8b3e5d3-7129-4b55-b4d4-1845e16e0548', 'c111de6e-396c-4940-bd95-394af78076db', 'Alimentos', 430.00, 'OUTPUT', '2024-05-02', 'PENSION', 'LH/V'),
+    ('d64aad58-4419-4de9-9b92-2bb79ba2f2b4', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 34.98, 'OUTPUT', '2024-05-03', 'TELEPHONY', 'Claro'),
+    ('d315a17e-5297-4223-8100-1a34ceadf1f5', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplementação alimentar', 398.05, 'OUTPUT', '2024-05-06', 'SUPPLEMENTATION', 'Amazon'),
+    ('6bcdaefc-6f6c-4917-a2e6-f848afeb77a3', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada de investimento na bolsa', 504.67, 'DISINVESTMENT', '2024-05-06', 'BOLSA', 'XP Investimentos'),
+    ('abf8361c-180f-4dd8-9236-87cb7781bf92', 'c111de6e-396c-4940-bd95-394af78076db', 'Esqueci', 3.98, 'OUTPUT', '2024-05-14', 'OTHER', 'Esqueci'),
+    ('fbdcece3-f2b3-48fd-8ce0-5cb09a052ded', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2712.77, 'INPUT', '2024-05-15', 'WAGE', 'Invillia'),
+    ('dbd188ab-6783-4765-a0f6-1d126fc40c44', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-05-15', 'POUPANCA', 'Itaú'),
+    ('bebdf28a-3bd9-4026-b3d2-0961017c474d', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de celular', 745.55, 'OUTPUT', '2024-05-15', 'ELETRONIC', 'Motorola'),
+    ('1823d01b-c842-4fc2-9b9f-1df4ffad73a3', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-05-16', 'POUPANCA', 'Itaú'),
+    ('d82ea1bb-1512-4d1a-97fe-81ce407e8ca6', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada de investimento na bolsa', 498.28, 'DISINVESTMENT', '2024-05-17', 'BOLSA', 'XP Investimentos'),
+    ('a86e68d2-5f23-4171-829b-b5803cd149ab', 'c111de6e-396c-4940-bd95-394af78076db', 'Curso', 24.90, 'OUTPUT', '2024-05-17', 'EDUCATION', 'Udemy'),
+    ('c2121b71-65b4-495c-a63d-58b0b6ada3c6', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-05-17', 'POUPANCA', 'Itaú'),
+    ('bdf34fa8-ba4b-4424-9ad3-e1d9ed589d96', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada da poupança', 46.96, 'DISINVESTMENT', '2024-05-20', 'POUPANCA', 'Itaú'),
+    ('65aa5f04-c1d1-428b-8a36-0ff8f411dee8', 'c111de6e-396c-4940-bd95-394af78076db', 'Curso', 24.90, 'OUTPUT', '2024-05-20', 'EDUCATION', 'Udemy'),
+    ('d506f8a7-a23e-4920-b352-227af68c9ee0', 'c111de6e-396c-4940-bd95-394af78076db', 'Curso', 24.90, 'OUTPUT', '2024-05-20', 'EDUCATION', 'Udemy'),
+    ('76555a64-f6bc-4c70-bb23-771b7242a5ab', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-05-23', 'POUPANCA', 'Itaú'),
+    ('e0dd370e-993e-4c3e-a067-145c423740e6', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-05-23', 'POUPANCA', 'Itaú'),
+    ('3655fbc6-2dbd-4ec1-b78e-2d7547059828', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada da poupança', 2041.03, 'DISINVESTMENT', '2024-05-23', 'POUPANCA', 'Itaú'),
+    ('862d44b5-3dd4-41a8-8195-bd0abfdf7be7', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada da poupança', 991.89, 'DISINVESTMENT', '2024-05-23', 'POUPANCA', 'Itaú'),
+    ('fea01bd4-3f3b-499a-b097-7bb69acb6761', 'c111de6e-396c-4940-bd95-394af78076db', 'Cursos', 49.80, 'OUTPUT', '2024-05-23', 'EDUCATION', 'Udemy'),
+    ('cc4571a4-a990-4db0-8af6-24ed9b97e6f0', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de equipamento', 91.40, 'OUTPUT', '2024-05-23', 'ELETRONIC', 'Mercado Livre'),
+    ('a20b35fa-ca99-4e78-995f-7f6afd648266', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 500.00, 'INVESTMENT', '2024-05-24', 'POUPANCA', 'Itaú'),
+    ('241b1568-f0a3-4010-871c-15fe2e689789', 'c111de6e-396c-4940-bd95-394af78076db', 'Financiamento Jornalismo', 211.18, 'OUTPUT', '2024-05-24', 'EDUCATION', 'Caixa Econômica'),
+    ('5f6621f8-cfcb-4049-bccb-bca4a5fd4621', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 15.00, 'OUTPUT', '2024-05-28', 'TELEPHONY', 'Vivo'),
+    ('40b1cc2f-3691-4ee8-9fda-dfab0d41bd59', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2472.72, 'INPUT', '2024-05-31', 'WAGE', 'Invillia'),
+    ('ba610ad8-2953-4f4d-9b21-9e88759ff3dd', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 500.00, 'INVESTMENT', '2024-05-31', 'POUPANCA', 'Itaú'),
+    ('d07a9ee7-8a11-4725-be4a-dbd8f94a93e1', 'c111de6e-396c-4940-bd95-394af78076db', 'Assinatura Zwift', 84.73, 'OUTPUT', '2024-05-31', 'SPORT', 'Zwift'),
+    ('03e6af0c-9be2-4cb0-9615-b1e4ba9e8db7', 'c111de6e-396c-4940-bd95-394af78076db', 'Curso', 34.90, 'OUTPUT', '2024-05-31', 'EDUCATION', 'Udemy');
+
