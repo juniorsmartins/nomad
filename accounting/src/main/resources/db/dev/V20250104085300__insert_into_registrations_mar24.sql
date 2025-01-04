9463a5fad2beed014d7d1fc644d9f4a8fd2afd57
juniorsmartins <junior.soares.martins@proton.me>
2025-01-04 09:03:04 -0300

    FEAT: add novos dados.

---
+++ accounting/src/main/resources/db/dev/V20250104085300__insert_into_registrations_mar24.sql
@@ -0,0 +1,22 @@
+INSERT INTO registrations(
+    registration_id, cashbook_id, description, amount, type_operation, date_operation, cost_center, supplier
+)
+VALUES
+    ('75278e6c-3425-4616-8873-b3842b9901f9', 'c111de6e-396c-4940-bd95-394af78076db', 'Ração', 251.09, 'OUTPUT', '2024-03-04', 'PET', 'Cobasi'),
+    ('7f3d7f21-90a2-4e57-a8cb-045c66f34aa8', 'c111de6e-396c-4940-bd95-394af78076db', 'Livro', 70.07, 'OUTPUT', '2024-03-04', 'EDUCATION', 'Amazon'),
+    ('e5e053b9-5092-4470-8016-35c5641d755f', 'c111de6e-396c-4940-bd95-394af78076db', 'Alimentos', 425.00, 'OUTPUT', '2024-03-07', 'PENSION', 'LH/V'),
+    ('7e3beaf4-bfbe-42e7-838a-b6fbe8c8fef5', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 20.00, 'OUTPUT', '2024-03-07', 'TELEPHONY', 'Tim'),
+    ('799d0203-8179-4d8d-8e47-3fb1084dd211', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 500.00, 'INVESTMENT', '2024-03-11', 'POUPANCA', 'Itaú'),
+    ('7784e905-2fd7-4bdb-ba09-61f975c99420', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('a49e9c20-782a-414e-8c1b-a45b862a5716', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('281e6495-6766-4666-a87b-5267f59541dc', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('a7c5295e-903a-473c-9142-1dc323767e4a', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('5305a730-1a06-42ed-9328-c9d470b5c315', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('9c6b5021-764a-4cc9-9b2c-b50f86b3902d', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('b6e255a5-a83c-4e62-8b78-27e855e5007b', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('b56730f5-6090-4330-a374-3a789150ec76', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('9e725c8f-010f-442d-8a3f-5be282de699f', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('666e99ba-2ef6-41ad-a14b-eeb93d42f940', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('5643fb76-3648-45b1-917c-f5f758445bb5', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', ''),
+    ('', 'c111de6e-396c-4940-bd95-394af78076db', '', , 'OUTPUT', '2024-03-02', '', '');
+
