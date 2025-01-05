54f468ad701c2264391cdca3a13aacc2c490c327
juniorsmartins <junior.soares.martins@proton.me>
2025-01-05 13:06:30 -0300

    FEAT: add dados

---
+++ accounting/src/main/resources/db/dev/V20250105112900__insert_into_registrations_dez24.sql
@@ -0,0 +1,38 @@
+INSERT INTO registrations(
+    registration_id, cashbook_id, description, amount, type_operation, date_operation, cost_center, supplier
+)
+VALUES
+    ('babee402-f266-4489-ba51-903a04eab066', 'c111de6e-396c-4940-bd95-394af78076db', 'Alimentos', 450.00, 'OUTPUT', '2024-12-02', 'PENSION', 'LH/V'),
+    ('8746bb58-350c-483e-b94b-f8b15ce62c67', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 40.00, 'OUTPUT', '2024-12-02', 'TELEPHONY', 'Vivo'),
+    ('34fbe8b9-d0ae-4a9e-b53f-6bf35784f52e', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 500.00, 'INVESTMENT', '2024-12-02', 'POUPANCA', 'Itaú'),
+    ('21aceaed-048d-4e5e-abf5-a649b5ea722e', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplemento alimentar', 253.78, 'OUTPUT', '2024-12-02', 'SUPPLEMENTATION', 'Amazon'),
+    ('01ed73aa-a1dc-4835-a071-e33ba73432e3', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de dólar', 100.00, 'INVESTMENT', '2024-12-02', 'DOLAR', 'Nomad'),
+    ('d4f448fc-7c23-40b7-9162-09bc145c8dda', 'c111de6e-396c-4940-bd95-394af78076db', 'Cursos', 49.80, 'OUTPUT', '2024-12-04', 'EDUCATION', 'Udemy'),
+    ('0efe7681-212e-421e-a2cf-97824fa83409', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos na trilha', 8.00, 'OUTPUT', '2024-12-09', 'SPORT', 'Outros'),
+    ('84cdafb5-c120-41fb-9ce9-718f5476dd8d', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 119.60, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('cfb2c729-b054-43ea-b96b-9fbff8afa18b', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 25.88, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('1bcc1077-688c-44bf-a842-c57a8ded3ea3', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 24.90, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('1f7e7466-84af-4c43-ab40-5696733ba74a', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 49.80, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('e00f174a-29a3-46a7-b6a9-419de7e729cb', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 24.90, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('0f29e6ac-838d-4d4f-bf88-8f26f23f525d', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 29.90, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('3c53b6f5-2fb4-49fb-b1db-3cd052d3aed9', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 119.60, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('2e1ae60c-01ff-4750-9d54-2b7e5f46514f', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 184.83, 'INPUT', '2024-12-10', 'WAGE', 'Swille'),
+    ('89b779a6-f19c-4e2f-84f0-780e468b7f60', 'c111de6e-396c-4940-bd95-394af78076db', 'FIES - Financiamento Jornalismo', 210.93, 'OUTPUT', '2024-12-11', 'EDUCATION', 'Caixa Econômica'),
+    ('9521aa8a-8b46-43e7-907e-49e550b0b0b2', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 24.90, 'INPUT', '2024-12-12', 'WAGE', 'Swille'),
+    ('b8b5f590-c218-4b5f-aad6-ee789db1f1a6', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 49.80, 'INPUT', '2024-12-12', 'WAGE', 'Swille'),
+    ('72179518-0b81-47c4-ab05-a4a07871d519', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 124.60, 'INPUT', '2024-12-12', 'WAGE', 'Swille'),
+    ('0da2c16a-39f5-42d6-8226-dd96a05f11bb', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 114.33, 'INPUT', '2024-12-12', 'WAGE', 'Swille'),
+    ('b7a119b9-857f-4785-9e73-915c71b7eaeb', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 65.17, 'INPUT', '2024-12-12', 'WAGE', 'Swille'),
+    ('e121f679-6e63-4c8e-8c9a-43ef83391f12', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2712.77, 'INPUT', '2024-12-13', 'WAGE', 'Invillia'),
+    ('3073327d-5a44-4559-8120-d62d8314abbf', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de dólares', 150.00, 'INVESTMENT', '2024-12-13', 'DOLAR', 'Nomad'),
+    ('8894e1f7-3148-4549-a91e-6e1ccdba12d8', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de game', 165.60, 'OUTPUT', '2024-12-16', 'ENTERTAINMENT', 'Steam'),
+    ('2e215521-b31d-4da8-961d-85a994e54303', 'c111de6e-396c-4940-bd95-394af78076db', 'Pgto parcial de 13 salário dev pleno', 1795.13, 'INPUT', '2024-12-20', 'WAGE', 'Invillia'),
+    ('b29bb969-549a-494e-8999-0642240b848d', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de dólares', 150.00, 'INVESTMENT', '2024-12-20', 'DOLAR', 'Nomad'),
+    ('fa400a92-a920-4fee-bd9f-739cf5581d6a', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 900.00, 'INVESTMENT', '2024-12-20', 'POUPANCA', 'Itaú'),
+    ('c3a9c279-9a07-49e2-9cd8-6f305994e61e', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 40.00, 'OUTPUT', '2024-12-23', 'TELEPHONY', 'Vivo'),
+    ('0b0f8bd7-47f3-4b8a-8e3a-54f0a29a3c6c', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra game', 15.62, 'OUTPUT', '2024-12-24', 'ENTERTAINMENT', 'Steam'),
+    ('f362ba09-46fd-4eaf-9098-3369c42bc799', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra game', 28.24, 'OUTPUT', '2024-12-24', 'ENTERTAINMENT', 'Steam'),
+    ('b418fb57-5423-4dab-9882-363f5cd29507', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra game', 15.62, 'OUTPUT', '2024-12-26', 'ENTERTAINMENT', 'Steam'),
+    ('eeed5d6c-a3dd-4360-aba8-b2f16047ff8e', 'c111de6e-396c-4940-bd95-394af78076db', 'Médico Veterinário', 210.00, 'OUTPUT', '2024-12-30', 'PET', 'Clube dos Bichos'),
+    ('0f08e73a-263c-481c-9c8e-eafd9de34d81', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2472.72, 'INPUT', '2024-12-30', 'WAGE', 'Invillia');
+
