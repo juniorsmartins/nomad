d5e3ffa4d2956515b4886940ed35f947190d7bfa
juniorsmartins <junior.soares.martins@proton.me>
2025-01-05 09:16:42 -0300

    FEAT: add dados

---
+++ accounting/src/main/resources/db/dev/V20250105065900__insert_into_registrations_jun24.sql
@@ -0,0 +1,36 @@
+INSERT INTO registrations(
+    registration_id, cashbook_id, description, amount, type_operation, date_operation, cost_center, supplier
+)
+VALUES
+    ('539cd2ea-a972-4f86-86c5-732d11b63677', 'c111de6e-396c-4940-bd95-394af78076db', 'Ração', 178.49, 'OUTPUT', '2024-06-03', 'PET', 'Cobasi'),
+    ('6ff94581-ec94-4d24-a38e-80a63f7344a9', 'c111de6e-396c-4940-bd95-394af78076db', 'Alimentos', 430.00, 'OUTPUT', '2024-06-03', 'PENSION', 'LH/V'),
+    ('dc1e375a-c947-4371-934e-e81679d7f720', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplemento alimentar', 190.27, 'OUTPUT', '2024-06-03', 'SUPPLEMENTATION', 'Amazon'),
+    ('6819d150-4e14-48ac-96af-d0114c13b9bd', 'c111de6e-396c-4940-bd95-394af78076db', 'Corte de cabelo', 40, 'OUTPUT', '2024-06-03', 'BARBERSHOP', 'Barbearia do Careca'),
+    ('55aeae2c-0d3f-4c91-9800-c08a5587004e', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplemento alimentar', 77.95, 'OUTPUT', '2024-06-03', 'SUPPLEMENTATION', 'Amazon'),
+    ('49318897-df6a-4920-81b8-d739cd51a64e', 'c111de6e-396c-4940-bd95-394af78076db', 'Camiseta', 135.00, 'OUTPUT', '2024-06-03', 'CLOTHING', 'Insider'),
+    ('777c2c4a-9415-4df4-9ccc-1295afd19a2f', 'c111de6e-396c-4940-bd95-394af78076db', 'Pgto FGTS', 617.43, 'INPUT', '2024-06-03', 'FGTS', 'Governo Federal'),
+    ('367ed5a1-fe0a-4f61-ab05-3458423d7519', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 20.00, 'OUTPUT', '2024-06-04', 'TELEPHONY', 'Vivo'),
+    ('69a95906-c0dc-4878-92ac-21045f4df72b', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada da bolsa', 1029.23, 'DISINVESTMENT', '2024-06-04', 'BOLSA', 'XP Investimentos'),
+    ('fec0b8c5-9fd3-4203-9cef-4ef7d5328c79', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de smartwatch', 2573.00, 'OUTPUT', '2024-06-05', 'ELETRONIC', 'Mercado Livre'),
+    ('7dc89889-cb79-41a7-9f01-c997305fab1f', 'c111de6e-396c-4940-bd95-394af78076db', 'Retirada da bolsa', 1993.15, 'DISINVESTMENT', '2024-06-07', 'BOLSA', 'XP Investimentos'),
+    ('e4c99e83-7039-4164-be3e-b3eb04a6204d', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-06-10', 'POUPANCA', 'Itaú'),
+    ('6352e325-0768-406f-a1c8-7bfb1a26ed79', 'c111de6e-396c-4940-bd95-394af78076db', 'Financiamento Jornalismo - FIES', 206.78, 'OUTPUT', '2024-06-10', 'EDUCATION', 'Caixa Econômica'),
+    ('34fe3f54-bc46-4b1a-b58d-567bbb43bf62', 'c111de6e-396c-4940-bd95-394af78076db', 'Aquisição de roupas', 804.67, 'OUTPUT', '2024-06-10', 'CLOTHING', 'Mercado Livre'),
+    ('a8da0ec2-9e4d-4de4-9543-87b369678042', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 15.00, 'OUTPUT', '2024-06-11', 'TELEPHONY', 'Vivo'),
+    ('371abe22-12aa-465e-aa59-a2ca2d714f53', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 115.47, 'INPUT', '2024-06-11', 'WAGE', 'Swille'),
+    ('1ea2a8ba-6142-4602-a0de-1236ce42bbf1', 'c111de6e-396c-4940-bd95-394af78076db', 'Benefícios vales', 63.12, 'INPUT', '2024-06-11', 'WAGE', 'Swille'),
+    ('b3a93451-bc97-47d7-9fb8-7f47b23daa52', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de roupas', 87.04, 'OUTPUT', '2024-06-13', 'CLOTHING', 'Mercado Livre'),
+    ('26105024-f547-48b7-8fb9-b58fc57a2865', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2712.77, 'INPUT', '2024-06-14', 'WAGE', 'Invillia'),
+    ('a4728e9f-62b8-4965-ae58-70a98c1e11ac', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-06-17', 'POUPANCA', 'Itaú'),
+    ('3d347aad-abb2-40c7-a658-8d0f8e08b0cf', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-06-17', 'POUPANCA', 'Itaú'),
+    ('9cae1ca8-e89f-4634-9fd0-e223ccf8a972', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplemento alimentar', 99.56, 'OUTPUT', '2024-06-19', 'SUPPLEMENTATION', 'Mercado Livre'),
+    ('2e9bc3c7-86fb-48c2-af58-ed7b90a8d906', 'c111de6e-396c-4940-bd95-394af78076db', 'Assinatura Zwift', 118.54, 'OUTPUT', '2024-06-20', 'SPORT', 'Zwift'),
+    ('01b8083e-99d4-477c-908d-cbc710840a7d', 'c111de6e-396c-4940-bd95-394af78076db', 'Suplemento alimentar', 277.27, 'OUTPUT', '2024-06-20', 'SUPPLEMENTATION', 'Mercado Livre'),
+    ('a2d9a0e3-7a60-4636-86c9-87d7a8fe9466', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 300.00, 'INVESTMENT', '2024-06-21', 'POUPANCA', 'Itaú'),
+    ('a1d89dc7-c1e4-410d-9e9e-0cee1345cd93', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário de dev pleno', 2472.72, 'INPUT', '2024-06-28', 'WAGE', 'Invillia'),
+    ('aae328ca-3e41-4814-b31b-ccf03fe56c43', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1700.00, 'INVESTMENT', '2024-06-28', 'POUPANCA', 'Itaú'),
+    ('7ad8441b-a9dc-4223-9691-117a507b1f3f', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 1000.00, 'INVESTMENT', '2024-06-28', 'POUPANCA', 'Itaú'),
+    ('d0cf3d9b-28bd-4384-a6af-8c5092fec297', 'c111de6e-396c-4940-bd95-394af78076db', 'FIES - Financiamento Jornalismo', 206.78, 'OUTPUT', '2024-06-28', 'EDUCATION', 'Caixa Econômica'),
+    ('d0d043c2-2e74-4f0e-983f-f00c803325e7', 'c111de6e-396c-4940-bd95-394af78076db', 'IRPF - Estorno de imposto de renda', 1778.26, 'INPUT', '2024-06-28', 'IRPF', 'Receita Federal'),
+    ('37c8ab5b-44f1-46ef-a1f3-63237de67598', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 15.00, 'OUTPUT', '2024-06-28', 'TELEPHONY', 'Vivo');
+
