06aacdd129909c9f865c32d8ad1f3008f49ebd67
juniorsmartins <junior.soares.martins@proton.me>
2025-01-05 09:47:36 -0300

    FEAT: add dados

---
+++ accounting/src/main/resources/db/dev/V20250105091700__insert_into_registrations_ago24.sql
@@ -0,0 +1,30 @@
+INSERT INTO registrations(
+    registration_id, cashbook_id, description, amount, type_operation, date_operation, cost_center, supplier
+)
+VALUES
+    ('5f4d4b74-cedd-47ca-ae9c-ba22245be8da', 'c111de6e-396c-4940-bd95-394af78076db', 'FIES - Financiamento Jornalismo', 206.77, 'OUTPUT', '2024-08-02', 'EDUCATION', 'Caixa Econômica'),
+    ('1106fa25-9086-46d5-88b9-d66687acd82a', 'c111de6e-396c-4940-bd95-394af78076db', 'Alimentos', 450.00, 'OUTPUT', '2024-08-02', 'PENSION', 'LH/V'),
+    ('0a60d4f6-d0d7-43f4-b77e-6f7c6efa89af', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 15.00, 'OUTPUT', '2024-08-02', 'TELEPHONY', 'Tim'),
+    ('fa10cc5c-270c-4d71-b4a5-21d77a1e3cf3', 'c111de6e-396c-4940-bd95-394af78076db', 'Celular', 40.00, 'OUTPUT', '2024-08-02', 'TELEPHONY', 'Vivo'),
+    ('31de9453-4d50-4f56-b929-1f304c682609', 'c111de6e-396c-4940-bd95-394af78076db', 'Gasto em trilha', 16.00, 'OUTPUT', '2024-08-05', 'SPORT', 'Outros'),
+    ('2cd846be-b887-448b-bb5a-6f1f3841031d', 'c111de6e-396c-4940-bd95-394af78076db', 'Gasto em trilha', 28.50, 'OUTPUT', '2024-08-05', 'SPORT', 'Outros'),
+    ('b96ce2fd-2699-4554-88d3-62ac85ba15e6', 'c111de6e-396c-4940-bd95-394af78076db', 'Gasto em trilha', 14.50, 'OUTPUT', '2024-08-05', 'SPORT', 'Outros'),
+    ('f59beb11-150a-45fa-b36d-fecf6bb2583e', 'c111de6e-396c-4940-bd95-394af78076db', 'Gasto em trilha', 25.96, 'OUTPUT', '2024-08-05', 'SPORT', 'Outros'),
+    ('ec96678e-1c72-4875-88a1-1be05f910cfc', 'c111de6e-396c-4940-bd95-394af78076db', 'Compra de óculos esportivo', 180.48, 'OUTPUT', '2024-08-05', 'CLOTHING', 'Yopp'),
+    ('c9d26339-d654-46ea-9bf0-2546d731de44', 'c111de6e-396c-4940-bd95-394af78076db', 'Assinatura Zwift', 119.16, 'OUTPUT', '2024-08-05', 'SPORT', 'Zwift'),
+    ('9ed2b6ef-4be1-441e-84cf-26efe3a86149', 'c111de6e-396c-4940-bd95-394af78076db', 'Aquisição de roupas', 183.02, 'OUTPUT', '2024-08-08', 'CLOTHING', 'Mercado Livre'),
+    ('b6d68a4c-08e0-48e2-bfdb-f8d8285d371a', 'c111de6e-396c-4940-bd95-394af78076db', 'Corte de cabelo', 40.00, 'OUTPUT', '2024-08-12', 'BARBERSHOP', 'Barbearia do Careca'),
+    ('0e457428-ca8f-4953-9de8-1f9a04bac103', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos em trilha', 19.50, 'OUTPUT', '2024-08-12', 'SPORT', 'Outros'),
+    ('107d1121-0fc7-4373-833b-21245df67964', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos em trilha', 27.50, 'OUTPUT', '2024-08-12', 'SPORT', 'Outros'),
+    ('0c7b53dd-be97-4a09-8a41-73037781f5e3', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 2757.87, 'INVESTMENT', '2024-08-14', 'POUPANCA', 'Itaú'),
+    ('f80b0663-140c-4a9a-ba47-c4c1e676c70e', 'c111de6e-396c-4940-bd95-394af78076db', 'Ração e petiscos', 91.40, 'OUTPUT', '2024-08-14', 'PET', 'Cobasi'),
+    ('bfe148f9-2142-4839-9ea8-518151ceb723', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 2000.00, 'INVESTMENT', '2024-08-14', 'POUPANCA', 'Itaú'),
+    ('3c69d688-fc1c-4ed5-b746-a21ca6c508f2', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2712.77, 'INPUT', '2024-08-15', 'WAGE', 'Invillia'),
+    ('087d2a87-35b6-4aba-93db-c1829c0eb2b3', 'c111de6e-396c-4940-bd95-394af78076db', 'Poupança', 3000.00, 'INVESTMENT', '2024-08-15', 'POUPANCA', 'Itaú'),
+    ('14375279-e464-40b7-a339-87d5f95db0f3', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos em trilha', 18.50, 'OUTPUT', '2024-08-26', 'SPORT', 'Outros'),
+    ('bbe6e898-dafe-4127-b238-00bf4daacb43', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos em trilha', 5.00, 'OUTPUT', '2024-08-26', 'SPORT', 'Outros'),
+    ('74a0baf8-0751-4005-84e0-0d4635f8f743', 'c111de6e-396c-4940-bd95-394af78076db', 'Gastos em trilha', 23.50, 'OUTPUT', '2024-08-26', 'SPORT', 'Outros'),
+    ('67b2e9c9-faf2-4134-a14a-78e6e01e6735', 'c111de6e-396c-4940-bd95-394af78076db', 'Academia de musculação', 200.00, 'OUTPUT', '2024-08-26', 'SPORT', 'Personal Studio'),
+    ('0c5057b4-e25e-4da1-bd35-bf9401b4d09e', 'c111de6e-396c-4940-bd95-394af78076db', 'Salário dev pleno', 2472.72, 'INPUT', '2024-08-30', 'WAGE', 'Invillia');
+
+
