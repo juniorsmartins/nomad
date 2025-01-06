9efbfdb579d77a6bd7e2d77805104a1397686fb1
juniorsmartins <junior.soares.martins@proton.me>
2025-01-06 18:53:42 -0300

    FEAT: add novo projeto para trabalhar investimentos.

---
+++ accounting_investments/src/main/java/com/nomad/accounting_investments/AccountingInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.accounting_investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingInvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingInvestmentsApplication.class, args);
+	}
+
+}
---
+++ accounting_investments/src/test/java/com/nomad/accounting_investments/AccountingInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomad.accounting_investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
