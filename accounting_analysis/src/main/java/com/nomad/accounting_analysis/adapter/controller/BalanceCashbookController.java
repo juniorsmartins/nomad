9264fec6fe5b8f5d11ea68a0bfb23f7ca7fa0136
juniorsmartins <junior.soares.martins@proton.me>
2025-01-24 08:14:06 -0300

    REFACTOR: separação de métodos de Controller.

--- accounting_analysis/src/main/java/com/nomad/accounting_analysis/adapter/controller/BalanceCashbookController.java
+++ accounting_analysis/src/main/java/com/nomad/accounting_analysis/adapter/controller/BalanceCashbookController.java
@@ -2,11 +2,9 @@ package com.nomad.accounting_analysis.adapter.controller;
 
 import com.nomad.accounting_analysis.adapter.mapper.CashbookMapper;
 import com.nomad.accounting_analysis.application.port.input.BalanceCashbookInputPort;
-import com.nomad.accounting_analysis.application.port.input.SurplusCashbookInputPort;
 import com.nomad.accounting_analysis.config.exception.http404.CashbookNotFoundException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.Retry;
-import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 @Slf4j
 @RestController
@@ -30,59 +27,21 @@ public class BalanceCashbookController {
 
     private final BalanceCashbookInputPort balanceCashbookInputPort;
 
-    private final SurplusCashbookInputPort surplusCashbookInputPort;
-
     private final CashbookMapper cashbookMapper;
 
-    private final RetryRegistry retryRegistry;
-
-    @GetMapping(path = "/surplus/{id}")
-//    @Retry(name = "surplus2")
-    @CircuitBreaker(name = "default", fallbackMethod = "getFallbackAnnual")
-    public ResponseEntity<Object> surplus(@PathVariable(name = "id") final UUID cashbookId) {
-
-        log.info("Controller Surplus iniciado: {}", cashbookId);
-
-        var retry = retryRegistry.retry("surplus2");
-
-        Supplier<Object> supplier = Retry.decorateSupplier(retry, () -> {
-            return Optional.of(cashbookId)
-                    .map(surplusCashbookInputPort::surplus)
-                    .map(cashbookMapper::toBalanceCashbookDtoResponse)
-                    .orElseThrow();
-        });
-
-        Object response;
-
-        try {
-            response = supplier.get();
-            log.info("Controller Surplus concluído: {}", response);
-
-        } catch (Exception ex) {
-            log.info("Erro ao executar a operação com Retry", ex);
-            return ResponseEntity
-                    .internalServerError()
-                    .build();
-        }
-
-        return ResponseEntity
-                .ok()
-                .body(response);
-    }
-
     @GetMapping(path = "/{id}")
-//    @Retry(name = "default")
+    @Retry(name = "default")
     @CircuitBreaker(name = "default", fallbackMethod = "getFallbackAnnual")
     public ResponseEntity<Object> annual(@PathVariable(name = "id") final UUID cashbookId) {
 
-        log.info("Controller Annual iniciado: {}", cashbookId);
+        log.info("classe=controller metodo=annual - Iniciado com id: {}", cashbookId);
 
         var response = Optional.of(cashbookId)
                 .map(balanceCashbookInputPort::annual)
                 .map(cashbookMapper::toBalanceCashbookDtoResponse)
                 .orElseThrow();
 
-        log.info("Controller Annual concluído: {}", response);
+        log.info("classe=controller metodo=annual - Concluído com resposta: {}", response);
 
         return ResponseEntity
                 .ok()
@@ -90,18 +49,22 @@ public class BalanceCashbookController {
     }
 
     private ResponseEntity<String> getFallbackAnnual(final UUID cashbookId, CashbookNotFoundException exception) {
-        log.info("Não encontrado Cashbook com identificador: {}.", cashbookId, exception);
+        log.info("classe=controller metodo=getFallbackAnnual - Não encontrado Cashbook com identificador: {}.",
+                cashbookId, exception);
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(String.format("Não encontrado Cashbook com identificador: %s", cashbookId));
     }
 
     private ResponseEntity<String> getFallbackAnnual(final UUID cashbookId, RuntimeException exception) {
-        log.info("Falha ao buscar relatório anual com cashbookId: {}.", cashbookId, exception);
+        log.info("classe=controller metodo=getFallbackAnnual - Falha ao buscar relatório com cashbookId: {}.",
+                cashbookId, exception);
+
         return ResponseEntity
                 .internalServerError()
                 .body(String
-                        .format("Falha ao buscar relatório anual com cashbookId: %s. Tente novamente mais tarde.", cashbookId));
+                    .format("Falha ao buscar relatório anual com cashbookId: %s. Tente mais tarde.", cashbookId));
     }
 }
 
