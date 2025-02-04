72e42b25e419d0931529b54c1fedcea78995a0bb
juniorsmartins <junior.soares.martins@proton.me>
2025-02-04 17:05:23 -0300

    TEST: estudo de Cucumber.

--- accounting/src/test/java/cucumber/estudos/AprenderCucumber.java
+++ accounting/src/test/java/cucumber/estudos/AprenderCucumber.java
@@ -5,9 +5,14 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class AprenderCucumber {
 
     private int contador = 0;
+
+    private Date entrega = new Date();
 
     @Dado("que o valor do contador é {int}")
     public void que_o_valor_do_contador_é(Integer int1) {
@@ -25,18 +30,30 @@ public class AprenderCucumber {
     }
 
     @Dado("que a entrega será dia {int}\\/{int}\\/{int}")
-    public void que_a_entrega_será_dia(Integer int1, Integer int2, Integer int3) {
-
+    public void que_a_entrega_será_dia(Integer dia, Integer mes, Integer ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes - 1);
+        cal.set(Calendar.YEAR, ano);
+        entrega = cal.getTime();
     }
 
     @Quando("a entrega atrasar em {int} dias")
-    public void a_entrega_atrasar_em_dias(Integer int1) {
-
+    public void a_entrega_atrasar_em_dias(Integer dias) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(entrega);
+        cal.add(Calendar.DAY_OF_MONTH, dias);
+        entrega = cal.getTime();
     }
 
     @Então("a entrega será efetuada em {int}\\/{int}\\/{int}")
-    public void a_entrega_será_efetuada_em(Integer int1, Integer int2, Integer int3) {
+    public void a_entrega_será_efetuada_em(Integer dia, Integer mes, Integer ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes - 1);
+        cal.set(Calendar.YEAR, ano);
 
+        Assertions.assertEquals(cal.getTime().toString(), entrega.toString());
     }
 }
 
