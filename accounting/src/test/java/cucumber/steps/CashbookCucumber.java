b440b0cdbb67deab209532ba6370f6ec90feb838
juniorsmartins <junior.soares.martins@proton.me>
2025-02-05 09:36:51 -0300

    TEST: estudo de Cucumber.

--- accounting/src/test/java/cucumber/steps/CashbookCucumber.java
+++ accounting/src/test/java/cucumber/steps/CashbookCucumber.java
@@ -1,25 +1,65 @@
 package cucumber.steps;
 
 import com.nomad.accounting.adapter.dto.in.CashbookCreateDtoRequest;
+import com.nomad.accounting.adapter.dto.in.CashbookUpdateDtoRequest;
+import com.nomad.accounting.adapter.mapper.CashbookMapperIn;
+import com.nomad.accounting.application.core.domain.Cashbook;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
-import cucumber.utils.GenerateCPF;
+import org.mapstruct.factory.Mappers;
 
 import java.time.Year;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CashbookCucumber {
 
-    private CashbookCreateDtoRequest cashbookRequest;
+    private final CashbookMapperIn cashbookMapperIn = Mappers.getMapper(CashbookMapperIn.class);
 
-    @Dado("um Cashbook válido")
-    public void um_cashbook_válido() {
-        cashbookRequest = new CashbookCreateDtoRequest(Year
-                .of(ThreadLocalRandom
-                        .current()
-                        .nextInt(1000, 2025)),
-                GenerateCPF.gerarCPF());
+    private CashbookCreateDtoRequest cashbookCreateDtoRequest;
+
+    private Cashbook cashbook;
+
+    private CashbookUpdateDtoRequest cashbookUpdateDtoRequest;
+
+    @Dado("um CashbookCreateDtoRequest válido, com ano {int} e documento {string}")
+    public void um_cashbook_create_dto_request_válido_com_ano_e_documento(Integer ano, String documento) {
+        cashbookCreateDtoRequest = new CashbookCreateDtoRequest(Year.of(ano), documento);
+    }
+
+    @Quando("converter CashbookCreateDtoRequest para Cashbook")
+    public void converter_cashbook_create_dto_request_para_cashbook() {
+        cashbook = cashbookMapperIn.toCashbook(cashbookCreateDtoRequest);
+    }
+
+    @Então("receber um Cashbook válido, com ano {int} e documento {string}")
+    public void receber_um_cashbook_válido_com_ano_e_documento(Integer ano, String documento) {
+        assertNotNull(cashbook);
+        assertEquals(Cashbook.class, cashbook.getClass());
+        assertEquals(Year.of(ano), cashbook.getYearReference());
+        assertEquals(documento, cashbook.getDocument());
+    }
+
+    @Dado("um CashbookUpdateDtoRequest válido, com ano {int} e documento {string} e id {string}")
+    public void um_cashbook_update_dto_request_válido_com_ano_e_documento_e_id(Integer ano, String documento, String idUuid) {
+        cashbookUpdateDtoRequest = new CashbookUpdateDtoRequest(UUID.fromString(idUuid), Year.of(ano), documento);
+    }
+
+    @Quando("converter CashbookUpdateDtoRequest para Cashbook")
+    public void converter_cashbook_update_dto_request_para_cashbook() {
+        cashbook = cashbookMapperIn.toCashbook(cashbookUpdateDtoRequest);
+    }
+
+    @Então("receber um Cashbook válido, com ano {int} e documento {string} e id {string}")
+    public void receber_um_cashbook_válido_com_ano_e_documento_e_id(Integer ano, String documento, String idUuid) {
+        assertNotNull(cashbook);
+        assertEquals(Cashbook.class, cashbook.getClass());
+        assertEquals(UUID.fromString(idUuid), cashbook.getCashbookId());
+        assertEquals(Year.of(ano), cashbook.getYearReference());
+        assertEquals(documento, cashbook.getDocument());
     }
 
     @Quando("fizer Post")
@@ -27,8 +67,8 @@ public class CashbookCucumber {
 
     }
 
-    @Então("receber HTTP {int} Created")
-    public void receber_http_created(Integer int1) {
+    @Então("receber HTTP {int}")
+    public void receber_http(Integer int1) {
 
     }
 }
--- accounting/src/test/resources/features/steps/cashbook_cucumber.feature
+++ accounting/src/test/resources/features/steps/cashbook_cucumber.feature
@@ -5,9 +5,16 @@ Funcionalidade: Criar Cashbook
   Então receber HTTP 201 Created
 
 Cenário: Deve criar Cashbook com sucesso
-  Dado um Cashbook válido
+  Dado um CashbookCreateDtoRequest válido, com ano 1950 e documento "47361120008"
   Quando fizer Post
-  Então receber HTTP 201 Created
+  Então receber HTTP 201
 
+Cenário: Deve converter CashbookCreateDtoRequest para Cashbook
+  Dado um CashbookCreateDtoRequest válido, com ano 2025 e documento "30921744021"
+  Quando converter CashbookCreateDtoRequest para Cashbook
+  Então receber um Cashbook válido, com ano 2025 e documento "30921744021"
 
-
+Cenário: Deve converter CashbookUpdateDtoRequest para Cashbook
+  Dado um CashbookUpdateDtoRequest válido, com ano 2000 e documento "45104581004" e id "63ac9ef2-23b7-4b6b-92cb-0cf92cd5768a"
+  Quando converter CashbookUpdateDtoRequest para Cashbook
+  Então receber um Cashbook válido, com ano 2000 e documento "45104581004" e id "63ac9ef2-23b7-4b6b-92cb-0cf92cd5768a"
