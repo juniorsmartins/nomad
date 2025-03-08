08a5a8666d28a22cb05c251a8e3e62e69925d333
juniorsmartins <junior.soares.martins@proton.me>
2025-03-08 10:24:05 -0300

    TEST: add novo teste de Cucumber.

--- accounting/src/test/java/cucumber/steps/RegistrationControllerStep.java
+++ accounting/src/test/java/cucumber/steps/RegistrationControllerStep.java
@@ -2,6 +2,8 @@ package cucumber.steps;
 
 import com.nomad.accounting.adapter.dto.in.RegistrationCreateDtoRequest;
 import com.nomad.accounting.adapter.dto.out.RegistrationDtoResponse;
+import com.nomad.accounting.adapter.dto.out.RegistrationFindDtoResponse;
+import com.nomad.accounting.adapter.entity.RegistrationEntity;
 import com.nomad.accounting.adapter.repository.CashbookRepository;
 import com.nomad.accounting.adapter.repository.RegistrationRepository;
 import com.nomad.accounting.application.core.domain.enums.CostCenterEnum;
@@ -10,6 +12,7 @@ import cucumber.config.ConstantsTest;
 import io.cucumber.java.Before;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.it.Quando;
+import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Então;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -20,8 +23,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.Year;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,6 +46,8 @@ public class RegistrationControllerStep {
     private Response response;
 
     private RegistrationCreateDtoRequest registrationCreateDtoRequest;
+
+    private UUID idRegistration;
 
     @Before
     public void setUp() {
@@ -76,18 +83,62 @@ public class RegistrationControllerStep {
         assertThat(response).isNotNull();
     }
 
-    @Então("receberei do RegistrationController uma ResponseEntity com HTTP {int}")
+    @Entao("receberei do RegistrationController uma ResponseEntity com HTTP {int}")
     public void receberei_do_registration_controller_uma_response_entity_com_http(Integer status) {
         Assertions.assertEquals(status, response.getStatusCode());
     }
 
-    @Então("com um RegistrationDtoResponse no body, com amount {int} e typeOperation {string}")
+    @Entao("com um RegistrationDtoResponse no body, com amount {int} e typeOperation {string}")
     public void com_um_registration_dto_response_no_body_com_amount_e_type_operation(Integer amount, String typeOperation) {
         var body = response.as(RegistrationDtoResponse.class);
         assertThat(body).isNotNull();
         assertThat(body.cashbookId()).isNotNull();
         assertThat(body.registrationId()).isNotNull();
         assertThat(body.amount()).isEqualTo(BigDecimal.valueOf(amount));
+        assertThat(body.typeOperationEnum()).isEqualTo(TypeOperationEnum.valueOf(typeOperation));
+    }
+
+    @Dado("um UUID de Registration, com amount {int} e typeOperation {string}, referente a um Cashbook, com ano {int} e documento {string}")
+    public void um_uuid_de_registration_com_amount_e_type_operation_referente_a_um_cashbook_com_ano_e_documento(
+            Integer amount, String typeOperation, Integer yearReference, String document) {
+
+        var cashbookEntity = cashbookRepository
+                .findByYearReferenceAndDocument(Year.of(yearReference), document).get();
+
+        var registrationEntity = RegistrationEntity.builder()
+                .cashbook(cashbookEntity)
+                .amount(BigDecimal.valueOf(amount))
+                .typeOperationEnum(TypeOperationEnum.valueOf(typeOperation))
+                .description("Qualquer descrição")
+                .dateOperation(LocalDate.now())
+                .costCenterEnum(CostCenterEnum.WAGE)
+                .supplier("Qualquer fornecedor")
+                .build();
+
+        var registrationSave = registrationRepository.save(registrationEntity);
+        idRegistration = registrationSave.getRegistrationId();
+
+        assertThat(registrationSave).isNotNull();
+        assertThat(idRegistration).isNotNull();
+    }
+
+    @Quando("a requisição Get for feita no método findById")
+    public void a_requisicao_get_for_feita_no_metodo_find_by_id() {
+        response = RestAssured
+                .given().spec(requestSpecification)
+                    .contentType(ConstantsTest.CONTENT_TYPE_JSON)
+                .when()
+                    .get("/" + idRegistration);
+
+        assertThat(response).isNotNull();
+    }
+
+    @Entao("com um RegistrationFindDtoResponse no body, com amount {int} e typeOperation {string}")
+    public void com_um_registration_find_dto_response_no_body_com_amount_e_type_operation(Integer amount, String typeOperation) {
+        var body = response.as(RegistrationFindDtoResponse.class);
+        assertThat(body).isNotNull();
+        assertThat(body.registrationId()).isNotNull();
+        assertThat(body.amount().setScale(0, RoundingMode.HALF_UP)).isEqualTo(BigDecimal.valueOf(amount));
         assertThat(body.typeOperationEnum()).isEqualTo(TypeOperationEnum.valueOf(typeOperation));
     }
 }
--- accounting/src/test/resources/features/registration_controller.feature
+++ accounting/src/test/resources/features/registration_controller.feature
@@ -6,17 +6,21 @@ Funcionalidade: testar operações CRUD do RegistrationController
 
   Contexto:
     Dado um ambiente de teste de Accounting ativado
+    Dado cadastros de Cashbook, sem registrations, disponíveis na massa de dados
+      |   yearReference   |     document      |
+      |       1990        |   94090259070     |
+      |       2021        |   04623846083     |
+      |       1994        |   59022664082     |
 
     Cenario: Post para criar Registration com sucesso
-      Dado cadastros de Cashbook, sem registrations, disponíveis na massa de dados
-        |   yearReference   |     document      |
-        |       1990        |   94090259070     |
-        |       2021        |   04623846083     |
-        |       1994        |   59022664082     |
-      E uma requisição Post com RegistrationCreateDtoRequest válido, com amount 25 e typeOperation "OUTPUT"
+      Dado uma requisição Post com RegistrationCreateDtoRequest válido, com amount 25 e typeOperation "OUTPUT"
       Quando a requisição Post for feita no método create, para cashbook com ano 2021 e document "04623846083"
-      Então receberei do RegistrationController uma ResponseEntity com HTTP 201
+      Entao receberei do RegistrationController uma ResponseEntity com HTTP 201
       E com um RegistrationDtoResponse no body, com amount 25 e typeOperation "OUTPUT"
 
-
+    Cenario: Get para consultar Registration com sucesso
+      Dado um UUID de Registration, com amount 30 e typeOperation "INPUT", referente a um Cashbook, com ano 1994 e documento "59022664082"
+      Quando a requisição Get for feita no método findById
+      Entao receberei do RegistrationController uma ResponseEntity com HTTP 200
+      E com um RegistrationFindDtoResponse no body, com amount 30 e typeOperation "INPUT"
 
