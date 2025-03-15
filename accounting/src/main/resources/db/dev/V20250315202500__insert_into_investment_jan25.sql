e55f51f374080c8de971edf3ea5630ae7113996a
juniorsmartins <junior.soares.martins@proton.me>
2025-03-15 20:53:25 -0300

    FEAT: add dados

---
+++ accounting/src/main/resources/db/dev/V20250315202500__insert_into_investment_jan25.sql
@@ -0,0 +1,13 @@
+INSERT INTO investments(
+    investment_id, cashbook_id, description, amount, type_action, date_operation, category, supplier
+)
+VALUES
+    ('1c3e8c58-2d0c-4cf3-a9ec-03fc72ea6559', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Cofrinho', 500.00, 'INVESTMENT', '2025-01-03', 'CDB', 'Itaú'),
+    ('9cf1ae6c-5d9d-47d5-863c-5520434d54ea', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Moeda', 120.00, 'INVESTMENT', '2025-01-03', 'DOLAR', 'Nomad'),
+    ('8a1ca1b9-4b3d-459e-8fb9-cb72a69bf105', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Poupança', 100.00, 'INVESTMENT', '2025-01-03', 'POUPANCA', 'Itaú'),
+    ('56e8d6f2-30b5-430b-964f-c2da5dac8b3f', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Moeda', 150.00, 'INVESTMENT', '2025-01-10', 'DOLAR', 'Nomad'),
+    ('f91232d3-0aa4-43c9-8727-4c7fda88c608', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Moeda', 130.00, 'INVESTMENT', '2025-01-22', 'DOLAR', 'Nomad'),
+    ('b922546b-59ca-4868-93d0-05bcc1cac540', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Poupança', 10000.00, 'DISINVESTMENT', '2025-01-24', 'POUPANCA', 'Itaú'),
+    ('cbccbe23-c95b-4549-b6dc-af1c3627d05f', '9381810c-f6b5-4480-a1c5-d38fb3bf2972', 'Cofrinho', 10000.00, 'INVESTMENT', '2025-01-24', 'CDB', 'Itaú')
+;
+
